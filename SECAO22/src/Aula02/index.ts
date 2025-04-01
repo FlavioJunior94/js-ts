e97af732bcbdf5920fa29aadf37f123787af4130
FlavioJunior94 <fjunior@yahoo.com.br>
2025-04-01 15:31:13 -0300

    SECAO 22 INICIADA

---
+++ SECAO22/src/Aula02/index.ts
@@ -0,0 +1,16 @@
+const arrayNumeros: Array<number> = [1,2,3,4,5,6];
+console.log(arrayNumeros);
+
+async function promiseAsync(){
+    return 1;
+}
+
+function minhaPromise(){
+    return new Promise((resolve,reject) => {
+        setTimeout(()=>{
+            resolve(1);
+        },1000);
+    });
+}
+
+promiseAsync().then((resultado) => console.log(resultado+1));
